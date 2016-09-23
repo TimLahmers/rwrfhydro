7d1eca225ca3a19fa24219303be21bec5e63d0e2
TimLahmers <TimothyLahmers@email.arizona.edu>
2016-09-23 10:54:32 -0700

    Update Documentation for PlotFluxCompareMultiVal
    Documentation of new function added (from old function) and function text copied from PlotFluxCompare.

--- R/visualize_modeleval.R
+++ R/visualize_modeleval.R
@@ -121,7 +121,151 @@ PlotFluxCompare <- function(strDf.obs, strCol.obs="q_cms",
         }
 }
 
+#' Plot time series comparing modeled and observed fluxes, for multiple models
+#' 
+#' \code{PlotFluxCompareMultiVal} plots a time series of an observed flux (e.g.,
+#' streamflow, ET) and up to 6 modelled fluxes.
+#' 
+#' \code{PlotFluxCompareMultiVal} reads modelled and observed dataframes (e.g., as
+#' generated from \code{\link{ReadFrxstPts}} and \code{\link{ReadUsgsGage}}) and
+#' plot the time series and summary statistics. The tool will subset data to
+#' matching time periods (e.g., if the observed data is at 5-min increments and
+#' modelled data is at 1-hr increments, the tool will subset the observed data
+#' to select only observations on the matching hour break).
+#' 
+#' @param strDf.obs The OBSERVED flux time series dataframe (e.g., output from
+#'   \code{\link{ReadUsgsGage}}). The dataframe must contain a column of flux
+#'   values and a POSIXct column.
+#' @param strCol.obs The name of the column containing the flux values for the
+#'   OBSERVED dataframe (DEFAULT="q_cms").
+#' @param strDf.mod1 The FIRST MODEL flux time series dataframe (e.g., output
+#'   from \code{\link{ReadFrxstPts}}). The dataframe must contain a column of
+#'   flux values and a POSIXct column.
+#' @param strCol.mod1 The name of the column containing the FIRST MODEL flux
+#'   values (DEFAULT="q_cms").
+#' @param strDf.mod2 The SECOND MODEL flux time series dataframe (e.g., output
+#'   from \code{\link{ReadFrxstPts}}). The dataframe must contain a column of
+#'   flux values and a POSIXct column.
+#' @param strCol.mod2 The name of the column containing the SECOND MODEL flux
+#'   values (DEFAULT="q_cms").
+#' @param strDf.mod3 The THIRD MODEL flux time series dataframe (e.g., output
+#'   from \code{\link{ReadFrxstPts}}). The dataframe must contain a column of
+#'   flux values and a POSIXct column.
+#' @param strCol.mod3 The name of the column containing the THIRD MODEL flux
+#'   values (DEFAULT="q_cms").
+#' @param strDf.mod4 The FOURTH MODEL flux time series dataframe (e.g., output
+#'   from \code{\link{ReadFrxstPts}}). The dataframe must contain a column of
+#'   flux values and a POSIXct column.
+#' @param strCol.mod4 The name of the column containing the FOURTH MODEL flux
+#'   values (DEFAULT="q_cms").
+#' @param strDf.mod5 The FIFTH MODEL flux time series dataframe (e.g., output
+#'   from \code{\link{ReadFrxstPts}}). The dataframe must contain a column of
+#'   flux values and a POSIXct column.
+#' @param strCol.mod5 The name of the column containing the FIFTH MODEL flux
+#'   values (DEFAULT="q_cms").
+#' @param strDf.mod6 The SIXTH MODEL flux time series dataframe (e.g., output
+#'   from \code{\link{ReadFrxstPts}}). The dataframe must contain a column of
+#'   flux values and a POSIXct column.
+#' @param strCol.mod6 The name of the column containing the SIXTH MODEL flux
+#'   values (DEFAULT="q_cms").
+#' @param stdate Start date for plot/statistics (DEFAULT=NULL, all records will
+#'   be used). Date MUST be specified in POSIXct format with appropriate
+#'   timezone (e.g., as.POSIXct("2013-05-01 00:00:00", format="\%Y-\%m-\%d
+#'   \%H:\%M:\%S", tz="UTC"))
+#' @param enddate End date for plot/statistics (DEFAULT=NULL, all records will
+#'   be used). Date MUST be specified in POSIXct format with appropriate
+#'   timezone (e.g., as.POSIXct("2013-05-01 00:00:00", format="\%Y-\%m-\%d
+#'   \%H:\%M:\%S", tz="UTC"))
+#' @param logy (TRUE or FALSE) Optional flag to set the y-axis to log-scale
+#'   (DEFAULT=FALSE).
+#' @param labelObs Optional label for the observed streamflow
+#'   (DEFAULT="Observed")
+#' @param labelMod1 Optional label for the FIRST MODEL (DEFAULT="Model 1")
+#' @param labelMod2 Optional label for the SECOND MODEL (DEFAULT="Model 2")
+#' @param labelMod3 Optional label for the THIRD MODEL (DEFAULT="Model 3")
+#' @param labelMod4 Optional label for the FOURTH MODEL (DEFAULT="Model 4")
+#' @param labelMod5 Optional label for the FIFTH MODEL (DEFAULT="Model 5")
+#' @param labelMod6 Optional label for the SIXTH MODEL (DEFAULT="Model 6")
+#' @param title Optional for the plot (DEFAULT="Observed and Modelled Fluxes")
+#' @return A plot of the hydrographs.
+#'   
+#' @examples
+#' ## Take a time series of observed 5-minute streamflow values for Fourmile
+#' ## Creek (obsStr5min.fc) and two model runs (mod1Str1h.fc, mod2Str1h.fc), 
+#' ## all with streamflow columns named "q_cms", and plot the the hydrographs 
+#' ## for all three over the May-June snowmelt period.
+#' 
+#' \dontrun{
+#' PlotFluxCompareMultiVal(obsStr5min.fc, "q_cms", modStrh.chrt.fc, "q_cms", 
+#'                 strDf.mod2=modStrh.allrt.fc, strCol.mod2="q_cms",
+#'                 labelObs="Observed Fourmile Creek at Orodell",
+#'                 labelMod1="Channel Routing Only", labelMod2="All Routing",
+#'                 title="Streamflow: Fourmile Creek",
+#'                 stdate=as.POSIXct("2013-05-01 00:00:00", 
+#'                                   format="%Y-%m-%d %H:%M:%S", tz="UTC"),
+#'                 enddate=as.POSIXct("2013-06-30 00:00:00",
+#'                                    format="%Y-%m-%d %H:%M:%S", tz="UTC"))
+#' }
+#' @export
 
+PlotFluxCompareMultiVal <- function(strDf.obs, strCol.obs="q_cms",
+                            strDf.mod1, strCol.mod1="q_cms",
+                            strDf.mod2=NULL, strCol.mod2="q_cms",
+                            stdate=NULL, enddate=NULL, logy=FALSE,
+                            labelObs="Observed", labelMod1="Model 1", labelMod2="Model 2",
+                            title="Observed and Modelled Fluxes") {
+    # PREP DATA
+    if (!is.null(stdate) && !is.null(enddate)) {
+        strDf.obs <- subset(strDf.obs, POSIXct>=stdate & POSIXct<=enddate)
+        strDf.mod1 <- subset(strDf.mod1, POSIXct>=stdate & POSIXct<=enddate)
+        if (!is.null(strDf.mod2)) {
+            strDf.mod2 <- subset(strDf.mod2, POSIXct>=stdate & POSIXct<=enddate)
+            }
+        ttext <- paste0(title, " (", stdate, " to ", enddate, ")")
+        }
+    else {
+        ttext <- title
+        }
+    strDf.obs$qcomp.obs <- strDf.obs[,strCol.obs]
+    strDf.mod1$qcomp.mod1 <- strDf.mod1[,strCol.mod1]
+    if (!is.null(strDf.mod2)) {
+        strDf.mod2$qcomp.mod2 <- strDf.mod2[,strCol.mod2]
+    }
+    strDf <- merge(strDf.obs[c("POSIXct","qcomp.obs")], strDf.mod1[c("POSIXct","qcomp.mod1")], by=c("POSIXct"))
+    if (!is.null(strDf.mod2)) {
+        strDf <- merge(strDf, strDf.mod2[c("POSIXct","qcomp.mod2")], by<-c("POSIXct"))
+        }
+    # STATS
+    nseflow1 <- round(Nse(strDf$qcomp.mod1, strDf$qcomp.obs), 2)
+    biasflow1 <- round(sum(strDf$qcomp.mod1-strDf$qcomp.obs, na.rm=TRUE)/sum(strDf$qcomp.obs, na.rm=TRUE) * 100, 1)
+    maxflow <- max(max(strDf$qcomp.obs, na.rm=TRUE), max(strDf$qcomp.mod1, na.rm=TRUE))
+    minflow <- min(min(strDf$qcomp.obs, na.rm=TRUE), min(strDf$qcomp.mod1, na.rm=TRUE))
+    if (!is.null(strDf.mod2)) {
+        maxflow <- max(maxflow, max(strDf$qcomp.mod2, na.rm=TRUE))
+        minflow <- min(minflow, min(strDf$qcomp.mod2, na.rm=TRUE))
+        nseflow2 <- round(Nse(strDf$qcomp.mod2, strDf$qcomp.obs), 2)
+        biasflow2 <- round(sum(strDf$qcomp.mod2-strDf$qcomp.obs, na.rm=TRUE)/sum(strDf$qcomp.obs, na.rm=TRUE) * 100, 1)
+        }
+    # PLOT
+    if (logy) {
+        with(strDf, plot(POSIXct, log10(qcomp.mod1), typ='l', log='y', col='green2', ylab=paste0(strCol.mod1),
+                                main=ttext, ylim=c(minflow,maxflow)))
+        }
+    else {
+        with(strDf, plot(POSIXct, qcomp.mod1, typ='l', col='green2', ylab=paste0(strCol.mod1),
+                                main=ttext, ylim=c(minflow,maxflow)))
+        }
+    if (!is.null(strDf.mod2)) { with(strDf, lines(POSIXct, qcomp.mod2, col='blue')) }
+    with(strDf, lines(POSIXct, qcomp.obs, col='black'))
+    if (!is.null(strDf.mod2)) {
+        legend('topright', c(labelMod1, labelMod2, labelObs), col=c('green2','blue','black'), lty=c(1,1,1), bg="white")
+        mtext(c(paste0("MODEL1: NSE=", nseflow1, " Bias=", biasflow1, "%  MODEL2: NSE=", nseflow2, " Bias=", biasflow2, "%")), side=3, line=0.0, cex=0.9)
+        }
+    else {
+        legend('topright', c(labelMod1, labelObs), col=c('green2','black'), lty=c(1,1), bg="white")
+        mtext(c(paste0("MODEL: NSE=", nseflow1, " Bias=", biasflow1, "%")), side=3, line=0.0, cex=0.9)
+        }
+}
 
 #' Plot water balance from WRF-Hydro (w/NoahMP) output
 #' 
